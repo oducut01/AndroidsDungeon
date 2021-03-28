1712845189e9608ada409d80477cc55714b15cc3
Omar Ducut <oducut@academic.rrc.ca>
2021-03-27 20:19:28 -0500

    Added ActiveStorage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :salePrice, :msrp, :description, :quantity, presence: true
+  validates :salePrice, :msrp, numericality: { only_integer: true }
+  has_one_attached :image
 end
