8c3e90aa65166eea0ccb3d43e6effed56a6f5780
Omar Ducut <oducut@academic.rrc.ca>
2021-03-30 13:36:12 -0500

    Added Product Category filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,20 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show edit update destroy]
-
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = if !params[:category_id] || params[:category_id] == ""
+                  Product.all
+                else
+                  Product.where(category_id: params[:category_id])
+                end
   end
 
   # GET /products/1 or /products/1.json
-  def show; end
+  def show
+    @product = Product.find(params[:id])
+  end
 
   def search
     @products = Product.where("name LIKE ? OR description LIKE ?", "%#{params[:search_term]}%",
                               "%#{params[:search_term]}%")
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_product
-    @product = Product.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def product_params
-    params.require(:product).permit(:name, :salePrice, :msrp, :description, :quantity)
   end
 end
