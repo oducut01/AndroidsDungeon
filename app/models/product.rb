19763cf31d7a64d787b8660153f9f961a523047a
Omar Ducut <oducut@academic.rrc.ca>
2021-03-30 10:49:57 -0500

    Added Category MVC

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :name, :salePrice, :msrp, :description, :quantity, presence: true
   validates :salePrice, :msrp, numericality: { only_integer: true }
   has_one_attached :image
+  belongs_to :category
 end
