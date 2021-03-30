11d1cb9920319d070582a2b87ec1691ff3f74625
Omar Ducut <oducut@academic.rrc.ca>
2021-03-30 13:49:18 -0500

    Added search bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :products
   resources :products, only: :show do
     collection do
       get :search
