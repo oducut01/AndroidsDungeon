d519fb9542c658b7c2d037266723c8f76705ed31
Omar Ducut <oducut@academic.rrc.ca>
2021-03-31 12:24:29 -0500

    Added basic Stripe integration

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,9 +1,37 @@
 class CheckoutController < ApplicationController
   def create
-    @product = Product.find(params[:product_id])
+    product = Product.find(params[:product_id])
+
+    redirect_to root_path if product.nil?
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      success_url:          checkout_success_url,
+      cancel_url:           checkout_cancel_url,
+      line_items:           [
+        {
+          name:        product.name,
+          description: product.description,
+          amount:      product.salePrice,
+          currency:    "cad",
+          quantity:    1
+        },
+        {
+          name:        "GST",
+          description: "Goods and Services Tax",
+          amount:      (product.salePrice * 0.05).to_i,
+          currency:    "cad",
+          quantity:    1
+        }
+      ]
+    )
 
     respond_to do |format|
       format.js
     end
   end
 end
+
+def success; end
+
+def cancel; end
