c40ce65ea516210a095762c91fb5bd871df20f68
Omar Ducut <oducut@academic.rrc.ca>
2021-03-31 00:31:41 -0500

    Added bug fix for buy buttons

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,9 @@
 class CheckoutController < ApplicationController
-  def create; end
+  def create
+    @product = Product.find(params[:product_id])
 
-  def success; end
-
-  def cancel; end
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,6 @@ Rails.configuration.stripe = {
   publishable_key: ENV["PUBLISHABLE_KEY"],
   secret_key:      ENV["SECRET_KEY"]
 }
+# ENV[] hash allows us to access the OS environment variables!
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
