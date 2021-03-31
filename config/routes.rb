ef29161a973aecf54e6af1744528ad51be17c4fb
Omar Ducut <oducut@academic.rrc.ca>
2021-03-30 20:44:03 -0500

    Added Add/View button styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
       get :search
     end
   end
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
+
   root to: "products#index"
 end
