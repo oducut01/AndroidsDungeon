39ecaed24969146e2d25c9c0d3f85b92c5adfba1
Omar Ducut <oducut@academic.rrc.ca>
2021-03-31 20:01:05 -0500

    Added intial Cart code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session, :increment_visit_count
+  helper_method :visit_count
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+  end
+
+  def visit_count
+    session[:visit_count]
+  end
 end
